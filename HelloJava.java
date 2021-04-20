5240644e30f564a22cecbeb13f2ee43e39ee62e3
swenor <swenor27@yahoo.com>
2021-04-21 01:53:55 +0200

    Simple function added in FIELDS. Function called in main() body.

--- HelloJava.java
+++ HelloJava.java
@@ -1,8 +1,13 @@
 public class HelloJava {
 	
+    static void hi() {
+        System.out.println("Hi, it's me - Java.")
+    }
+
 	public static void main(String[] args {
 
 		System.out.println("Java says Hello!");
+        hi();
 
 	}
 
