dc5b0126946d31134be38f4dc2b96d60d01f3581
swenor27 <swenor27@yahoo.com>
2021-04-27 19:16:05 +0200

    HelloJava.java was renamed to Main.java. Content of Main.java was altered as well as Main method was inside a hi() function and it had to be taken out. It was wrong from the point of view of syntax.

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    static void hi() {
+        System.out.println("Hi, it's me - Java.");
+    }
+
+    public static void main(String[]args){
+        System.out.println("Java says Hello!");
+        hi();
+
+    }
+
+}
