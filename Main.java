86c23d7163ed792694495b7077ffe4c4b2362cdb
swenor27 <swenor27@yahoo.com>
2021-04-27 19:39:06 +0200

    Added a simple "if else" conditional statement that simulates conversation.

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     static void hi() {
@@ -5,9 +7,21 @@ public class Main {
     }
 
     public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Java says Hello!");
         hi();
 
+        System.out.println("What will you say? Say OK when it's ok. Say MEH, when it could be better.");
+        String phrase = sc.nextLine();
+
+        if (phrase == "OK" || phrase == "ok" || phrase == "Ok") {
+            System.out.println("Nice to hear that!");
+        } else if (phrase == "MEH" || phrase == "Meh") {
+            System.out.println("SO sorry!");
+        } else {
+            System.out.println("Sorry, I do not understand.");
+        }
     }
 
 }
